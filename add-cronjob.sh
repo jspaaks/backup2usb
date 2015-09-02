f92c9b735aa9428cc32e36049d132eb2111ec43e
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2015-09-02 10:32:37 +0200

    redirected stderr and stdout explicitly

--- add-cronjob.sh
+++ add-cronjob.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TMPDIR=`mktemp -d`
-crontab -l > ${TMPDIR}/cronjobs-old.txt
+crontab -l 1> ${TMPDIR}/cronjobs-old.txt 2>/dev/null
 cat ${TMPDIR}/cronjobs-old.txt cronjob.txt > ${TMPDIR}/cronjobs-new.txt
 crontab ${TMPDIR}/cronjobs-new.txt
 rm ${TMPDIR}/cronjobs-old.txt
