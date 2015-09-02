e38ac445e2739762d16e0620cf3bc48873be0ec6
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2015-09-02 16:31:45 +0200

    now backing up encrypted directory. Also the checks on /dev/sdb1 and /media/daisycutter/7e89d57b-ff95-4a20-9fd2-be228f1419c8 fail at the earliest point; added a check that verifies that /media/daisycutter/7e89d57b-ff95-4a20-9fd2-be228f1419c8/daisycutter-encrypted.bak/ exists (it doesnt during the first dry run); added a notification when the backup is done

--- backup-home-to-usbdisk.sh
+++ backup-home-to-usbdisk.sh
@@ -27,22 +27,11 @@ fi
 
 
 # define the source directory
-SRC=${HOME}/
+#SRC=${HOME}/
+SRC=/home/.ecryptfs/${USER}/
 
 # retrieve the device from the disk UUID
 USB_DISK_DEV=`blkid -U $USB_DISK_UUID -o device`
-
-# find where $USB_DISK_UUID is mounted
-USB_DISK_MNT=`findmnt -rn -S UUID=$USB_DISK_UUID -o TARGET`
-
-# define the destination directory
-DEST=${USB_DISK_MNT}/${USER}.bak/
-
-# url of the local copy of the last sync date
-lastSyncSRC=${HOME}"/.config/backup2usbdisk/last-sync.txt"
-
-# url of the remote copy of the last sync date
-lastSyncDEST=${DEST}"last-sync.txt"
 
 # check if the USB_DISK_UUID exists
 if [ -z "$USB_DISK_DEV" ]; then
@@ -50,13 +39,28 @@ if [ -z "$USB_DISK_DEV" ]; then
     exit -1
 fi
 
+# find where $USB_DISK_UUID is mounted
+USB_DISK_MNT=`findmnt -rn -S UUID=$USB_DISK_UUID -o TARGET`
+
 # check if USBDISK is mounted
 if [ -z "${USB_DISK_MNT}" ]; then
     notify-send "Error during backup" "$USB_DISK_DEV is not mounted. Aborting." -t 15000 -i ${HOME}/opt/backup2usbdisk/drive-removable-media-usb.svg
     exit -1
 fi
 
-notify-send "Starting the backup" "" -t 15000 -i ${HOME}/opt/backup2usbdisk/drive-removable-media-usb.svg
+# define the destination directory
+#DEST=${USB_DISK_MNT}/${USER}.bak/
+DEST=${USB_DISK_MNT}/${USER}-encrypted.bak/
+
+# url of the local copy of the last sync date
+lastSyncSRC=${HOME}"/.config/backup2usbdisk/last-sync.txt"
+
+# url of the remote copy of the last sync date
+lastSyncDEST=${DEST}"last-sync.txt"
+
+
+
+notify-send "Backup" "Starting now...it may take a while." -t 15000 -i ${HOME}/opt/backup2usbdisk/drive-removable-media-usb.svg
 
 # I'm not using compression even if that's faster. This is because rsync
 # sometimes has trouble with large files such as virtual machine disk
@@ -76,12 +80,22 @@ sudo rsync --delete    \
            ${DEST}
 
 
-theDate=`date`
 
-# write the current date and time to a file 'last-sync.txt' in ${DEST}
-echo "${SRC} was last synchronized with ${DEST} on ${theDate}" > ${lastSyncDEST}
+if [ -d ${DEST} ]; then
 
-# write the current date and time to a file 'last-sync.txt' in ${SRC}
-echo "${theDate}" > ${lastSyncSRC}
+    theDate=`date`
+
+    # write the current date and time to a file 'last-sync.txt' in ${DEST}
+    echo "${SRC} was last synchronized with ${DEST} on ${theDate}" > ${lastSyncDEST}
+
+    # write the current date and time to a file 'last-sync.txt' in ${SRC}
+    echo "${theDate}" > ${lastSyncSRC}
+
+    notify-send "Backup complete" "" -t 15000 -i ${HOME}/opt/backup2usbdisk/drive-removable-media-usb.svg
+
+else
+    echo "${DEST} does not exist; is this a dry run?"
+fi
+
 
 
