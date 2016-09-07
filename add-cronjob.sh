a6573b69dfb90293453bf712673c19608202007e
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-09-07 13:30:36 +0200

    added some comments explaining the steps in adding the crontab job

--- add-cronjob.sh
+++ add-cronjob.sh
@@ -7,10 +7,22 @@ set -o errexit
 # disallow unset variables
 set -o nounset
 
-
+# make a temporary directory
 TMPDIR=`mktemp -d`
+
+# print the existing crontab job, and pipe the output to a new file, cronjobs-old.txt
+# (make an empty crontab with crontab -e if you don't have one already)
 crontab -l 1> ${TMPDIR}/cronjobs-old.txt 2>/dev/null
+
+# concatenate the existing crontab jobs with the crontab job from cronjob.txt, and store
+# it in a new file cronjobs-new.txt
 cat ${TMPDIR}/cronjobs-old.txt cronjob.txt > ${TMPDIR}/cronjobs-new.txt
+
+# feed the new cronjobs to crontab so that it can store it in its list of jobs
 crontab ${TMPDIR}/cronjobs-new.txt
+
+# clean up the temporary files
 rm ${TMPDIR}/cronjobs-old.txt
 rm ${TMPDIR}/cronjobs-new.txt
+rm -rf ${TMPDIR}
+
